4838492e05bd2e8bc90265e679624f7f063d0e5a
DevEMCN <devemcn@gmail.com>
2017-03-23 16:08:09 +0000

    throw speed is now determined by hand, shoulder and elbow positions

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -78,7 +78,7 @@ public class Shoot : MonoBehaviour
     public int counter_cooldown = 0;
     void FixedUpdate()
     {
-        counter_cooldown--;
+        counter_cooldown--; // delay the amount of times you can shoot to once per movement
         IsAvailable = _sensor.IsAvailable;
 
         if (_bodyFrameReader != null)
@@ -95,11 +95,14 @@ public class Shoot : MonoBehaviour
                     
                    
 
-                    /* Shoot ball on Tap */
+                    /* get instances of the elbow and shoulders and head*/
+                    /*  insert left elbow and shoulder here for 2 hand throw*/
                     Windows.Kinect.Joint elbow = body.Joints[JointType.ElbowLeft];
                     Windows.Kinect.Joint shoulder = body.Joints[JointType.ShoulderLeft];
-
-                    //if (Input.GetButton("Fire1") && !thrown && availableShots > 0)
+             
+                    Windows.Kinect.Joint hand = body.Joints[JointType.HandLeft];
+                    // if the right elbow goes above the right shoulder the power meter becomes
+                    // available 
                     if (elbow.Position.Y > shoulder.Position.Y) {
                         /* Move Meter Arrow */
 
@@ -121,17 +124,24 @@ public class Shoot : MonoBehaviour
                         }
                         if (body.HandLeftState == HandState.Open && (counter_cooldown <= 0) && availableShots > 0)
                         {
-                            counter_cooldown = 50;
+                            counter_cooldown = 100; // delay next available shot
                             thrown = true;
                             availableShots--;
                             availableShotsGO.GetComponent<GUIText>().text = availableShots.ToString();
 
                             ballClone = Instantiate(ball, ballPos, transform.rotation) as GameObject;
-                            throwSpeed.y = throwSpeed.y + arrow.transform.position.x;
-                            throwSpeed.z = throwSpeed.z + arrow.transform.position.x;
-
+                            //throwSpeed.y = throwSpeed.y + arrow.transform.position.x;
+                            //throwSpeed.z = throwSpeed.z + arrow.transform.position.x;
+                            // justification for following code.
+                            // because the ball can only be thrown when your elbow is above your shoulder
+                            // the only way to move your hand closer to your shoulder would be to bend your elbow back
+                            // in a throwing motion
+                            // hence the throw power is determined by the distance between the height of the hand and
+                            // height of the shoulder
+                            throwSpeed.y = throwSpeed.y + (hand.Position.Y - shoulder.Position.Y) * 2;
+                            throwSpeed.z = throwSpeed.z + (hand.Position.Y - shoulder.Position.Y) * 2;
                             ballClone.GetComponent<Rigidbody>().AddForce(throwSpeed, ForceMode.Impulse);
-                            //GetComponent<AudioSource>().Play();
+                            
 
                         }
                     }
