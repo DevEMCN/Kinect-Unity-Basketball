16bb621bb4a415c0df5ef16ae66c44478c13fedc
DevEMCN <devemcn@gmail.com>
2017-03-24 17:20:27 +0000

    added functionality for multiplayer, lots of bugs and no kinect

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -2,22 +2,35 @@
 using System.Collections;
 using Windows.Kinect;
 using System.Linq;
+using System;
 
 public class Shoot : MonoBehaviour
 {
-    public GameObject ball;
-    private Vector3 throwSpeed = new Vector3(0, 26, 40); //This value is a sure basket
-    public Vector3 ballPos;
-    private bool thrown = false;
-    private GameObject ballClone;
+    // player 1 objects 
+    public GameObject player1Ball;
+    private Vector3 player1ThrowSpeed = new Vector3(0, 26, 40); //This value is a sure basket
+    public Vector3 player1BallPos;
+    private bool player1Thrown = false;
+    private GameObject player1BallClone;
+    public GameObject player1AvailableShotsGO;
+    private int player1AvailableShots = 5;
+    public GameObject player1Meter;
+    public GameObject player1Arrow;
+    private float player1ArrowSpeed = 0.3f; //Difficulty
+    private bool player1Right = true;
 
-    public GameObject availableShotsGO;
-    private int availableShots = 5;
-
-    public GameObject meter;
-    public GameObject arrow;
-    private float arrowSpeed = 0.3f; //Difficulty
-    private bool right = true;
+    // player 2 objects
+    public GameObject player2Ball;
+    private Vector3 player2ThrowSpeed = new Vector3(0, 26, 40); //This value is a sure basket
+    public Vector3 player2BallPos;
+    private bool player2Thrown = false;
+    private GameObject player2BallClone;
+    //public GameObject player2AvailableShotsGO;
+    private int player2AvailableShots = 5;
+    public GameObject player2Meter;
+    public GameObject player2Arrow;
+    private float player2ArrowSpeed = 0.3f; //Difficulty
+    private bool player2Right = true;
 
     public GameObject gameOver;
 
@@ -105,61 +118,127 @@ public class Shoot : MonoBehaviour
                     // available 
                     if (elbow.Position.Y > shoulder.Position.Y) {
                         /* Move Meter Arrow */
+                        /* Move Meter Arrow player 1*/
+                        if (player1Arrow.transform.position.x < 24f && player1Right)
+                        {
+                            player1Arrow.transform.position += new Vector3(player1ArrowSpeed, 0, 0);
+                        }
+                        if (player1Arrow.transform.position.x >= 24f)
+                        {
+                            player1Right = false;
+                        }
+                        if (player1Right == false)
+                        {
+                            player1Arrow.transform.position -= new Vector3(player1ArrowSpeed, 0, 0);
+                        }
+                        if (player1Arrow.transform.position.x <= 14.5f)
+                        {
+                            player1Right = true;
+                        }
 
-                        if (arrow.transform.position.x < 4.7f && right)
+                        /* move meter arrow player 2*/
+                        if (player2Arrow.transform.position.x < -14.3f && player2Right)
                         {
-                            arrow.transform.position += new Vector3(arrowSpeed, 0, 0);
+                            player2Arrow.transform.position += new Vector3(player2ArrowSpeed, 0, 0);
                         }
-                        if (arrow.transform.position.x >= 4.7f)
+                        if (player2Arrow.transform.position.x >= -14.3f)
                         {
-                            right = false;
+                            player2Right = false;
                         }
-                        if (right == false)
+                        if (player2Right == false)
                         {
-                            arrow.transform.position -= new Vector3(arrowSpeed, 0, 0);
+                            player2Arrow.transform.position -= new Vector3(player2ArrowSpeed, 0, 0);
                         }
-                        if (arrow.transform.position.x <= -4.7f)
+                        if (player2Arrow.transform.position.x <= -23.7f)
                         {
-                            right = true;
+                            player2Right = true;
                         }
-                        if (body.HandLeftState == HandState.Open && (counter_cooldown <= 0) && availableShots > 0)
-                        {
-                            counter_cooldown = 100; // delay next available shot
-                            thrown = true;
-                            availableShots--;
-                            availableShotsGO.GetComponent<GUIText>().text = availableShots.ToString();
 
-                            ballClone = Instantiate(ball, ballPos, transform.rotation) as GameObject;
-                            //throwSpeed.y = throwSpeed.y + arrow.transform.position.x;
-                            //throwSpeed.z = throwSpeed.z + arrow.transform.position.x;
-                            // justification for following code.
-                            // because the ball can only be thrown when your elbow is above your shoulder
-                            // the only way to move your hand closer to your shoulder would be to bend your elbow back
-                            // in a throwing motion
-                            // hence the throw power is determined by the distance between the height of the hand and
-                            // height of the shoulder
-                            throwSpeed.y = throwSpeed.y + (hand.Position.Y - shoulder.Position.Y) * 2;
-                            throwSpeed.z = throwSpeed.z + (hand.Position.Y - shoulder.Position.Y) * 2;
-                            ballClone.GetComponent<Rigidbody>().AddForce(throwSpeed, ForceMode.Impulse);
-                            
+                        /* Shoot ball on Tap */
 
+                        if (Input.GetButton("Fire1") && !player1Thrown && player1AvailableShots > 0)
+                        {
+                            player1Thrown = true;
+                            player1AvailableShots--;
+                            player1AvailableShotsGO.GetComponent<GUIText>().text = player1AvailableShots.ToString();
+
+                            player1BallClone = Instantiate(player1Ball, player1BallPos, transform.rotation) as GameObject;
+                            player1ThrowSpeed.y = player1ThrowSpeed.y + player1Arrow.transform.position.x;
+                            player1ThrowSpeed.z = player1ThrowSpeed.z + player1Arrow.transform.position.x;
+                            player1ThrowSpeed.x = -10; // players are out to the side 
+
+                            player1BallClone.GetComponent<Rigidbody>().AddForce(player1ThrowSpeed, ForceMode.Impulse);
+                            //GetComponent<AudioSource>().Play();
                         }
+                        if (Input.GetKeyDown(KeyCode.Space) /*&& !player1Thrown &&*/ /*player1AvailableShots > 0*/)
+                        {
+                            player2Thrown = true;
+                            player2AvailableShots--;
+                            //player2AvailableShotsGO.GetComponent<GUIText>().text = player2AvailableShots.ToString();
+
+                            player2BallClone = Instantiate(player2Ball, player2BallPos, transform.rotation) as GameObject;
+                            player2ThrowSpeed.y = Math.Abs(player2ThrowSpeed.y) + Math.Abs(player2Arrow.transform.position.x);
+                            player2ThrowSpeed.z = Math.Abs(player2ThrowSpeed.z) + Math.Abs(player2Arrow.transform.position.x);
+                            player2ThrowSpeed.x = 25; // players are out to the side 
+
+                            player2BallClone.GetComponent<Rigidbody>().AddForce(player2ThrowSpeed, ForceMode.Impulse);
+                            //GetComponent<AudioSource>().Play();
+                        }
+                        //if (body.HandLeftState == HandState.Open && (counter_cooldown <= 0) && availableShots > 0)
+                        //{
+                        //    counter_cooldown = 100; // delay next available shot
+                        //    thrown = true;
+                        //    availableShots--;
+                        //    availableShotsGO.GetComponent<GUIText>().text = availableShots.ToString();
+
+                        //    ballClone = Instantiate(ball, ballPos, transform.rotation) as GameObject;
+                        //    //throwSpeed.y = throwSpeed.y + arrow.transform.position.x;
+                        //    //throwSpeed.z = throwSpeed.z + arrow.transform.position.x;
+                        //    // justification for following code.
+                        //    // because the ball can only be thrown when your elbow is above your shoulder
+                        //    // the only way to move your hand closer to your shoulder would be to bend your elbow back
+                        //    // in a throwing motion
+                        //    // hence the throw power is determined by the distance between the height of the hand and
+                        //    // height of the shoulder
+                        //    throwSpeed.y = throwSpeed.y + (hand.Position.Y - shoulder.Position.Y) * 2;
+                        //    throwSpeed.z = throwSpeed.z + (hand.Position.Y - shoulder.Position.Y) * 2;
+                        //    ballClone.GetComponent<Rigidbody>().AddForce(throwSpeed, ForceMode.Impulse);
+
+
+                        //}
                     }
-                    
+
 
                     /* Remove Ball when it hits the floor */
 
-                    if (ballClone != null && ballClone.transform.position.y < -16)
+                    if (player1BallClone != null && player1BallClone.transform.position.y < -16)
                     {
-                        Destroy(ballClone);
-                        thrown = false;
-                        throwSpeed = new Vector3(0, 26, 40);//Reset perfect shot variable
+                        Destroy(player1BallClone);
+                        player1Thrown = false;
+                        player1ThrowSpeed = new Vector3(0, 26, 40);//Reset perfect shot variable
 
                         /* Check if out of shots */
 
-                        if (availableShots == 0)
+                        if (player1AvailableShots == 0)
                         {
-                            arrow.GetComponent<Renderer>().enabled = false;
+                            player1Arrow.GetComponent<Renderer>().enabled = false;
+                            Instantiate(gameOver, new Vector3(0.31f, 0.2f, 0), transform.rotation);
+                            Invoke("restart", 2);
+                        }
+                    }
+                    /* Remove Ball when it hits the floor */
+
+                    if (player2BallClone != null && player2BallClone.transform.position.y < -16)
+                    {
+                        Destroy(player1BallClone);
+                        player2Thrown = false;
+                        player2ThrowSpeed = new Vector3(0, 26, 40);//Reset perfect shot variable
+
+                        /* Check if out of shots */
+
+                        if (player2AvailableShots == 0)
+                        {
+                            player2Arrow.GetComponent<Renderer>().enabled = false;
                             Instantiate(gameOver, new Vector3(0.31f, 0.2f, 0), transform.rotation);
                             Invoke("restart", 2);
                         }
@@ -180,10 +259,7 @@ public class Shoot : MonoBehaviour
         Application.LoadLevel(Application.loadedLevel);
     }
 
-    static float RescalingToRangesB(float scaleAStart, float scaleAEnd, float scaleBStart, float scaleBEnd, float valueA)
-    {
-        return (((valueA - scaleAStart) * (scaleBEnd - scaleBStart)) / (scaleAEnd - scaleAStart)) + scaleBStart;
-    }
+    
 
     void OnApplicationQuit()
     {
